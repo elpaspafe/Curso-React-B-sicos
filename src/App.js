a934a95b523f9b78112f0356b79df4eaf70503df
Jose <elpaspafe@gmail.com>
2022-08-19 11:15:48 +0200

    Vídeo 16: Hook useState

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {EventosES6, EventosES7, MasSobreEventos} from "./components/Eventos";
 import ComunicacionComponentes from "./components/ComunicacionComponentes";
 import CicloVida from "./components/CicloVida";
 import AjaxApis from "./components/AjaxApis";
+import ContadorHooks from "./components/contadorHooks";
 
 function App() {
   return (
@@ -60,6 +61,8 @@ function App() {
         <CicloVida />
         <hr />
         <AjaxApis />
+        <hr />
+        <ContadorHooks titulo="seguidores" />
       </section>
       </header>
     </div>
